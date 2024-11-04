da127d47e4c1e0c8303cf27930070c37a5a40001
FortNbreak <98566516+FortNbreak@users.noreply.github.com>
2024-11-04 16:37:50 -0500

    Create FontManager.cs

---
+++ NexusBootstrapperV2/Handlers/FontManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace NexusBootstrapperV2
+{
+    class FontManager
+    {
+        private string placeholder = "This code had to be removed due to it not working and it is no longer needed anyway.";
+    }
+}
